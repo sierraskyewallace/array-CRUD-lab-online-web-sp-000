5b3681112b5491a2a494fc7bdf4e1b65f903e4aa
Sierra Wallace <sskwallace@gmail.com>
2020-07-31 04:30:37 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  fallout_games = ["Fallout 3", "Fallout NV", "Fallout 4", "Fallout 76"]
 end
 
 def add_element_to_end_of_array(array, element)
+  array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  x = array.count
+  array[x-1]
 end
